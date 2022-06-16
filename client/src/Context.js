ac58035f009e04cc15764c8f9caf78bd6759e99b
Tarikul Hasan Shuvo <tarikul15-7944@diu.edu.bd>
2022-06-16 14:47:29 +0600

    change socket serve url

--- client/src/Context.js
+++ client/src/Context.js
@@ -5,8 +5,8 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 // const socket = io('http://localhost:5000');
-// const socket = io('https://warm-wildwood-81069.herokuapp.com');
-const socket = io('http://localhost:5000');
+// const socket = io('https://shuvo.biswasoxygen.com/');
+const socket = io('http://157.245.121.156:5000');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
